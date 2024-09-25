ed6a4608d169c832e4d689c19dc7cf956cc5d818
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-09-25 18:23:55 +0900

    Refactor : 리팩토링

--- app/src/main/java/com/example/tossapp/common/CommonSpacer.kt
+++ app/src/main/java/com/example/tossapp/common/CommonSpacer.kt
@@ -1,4 +1,4 @@
-package com.example.tossapp.ui.components
+package com.example.tossapp.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
--- app/src/main/java/com/example/tossapp/ui/screens/TabTossStockHome.kt
+++ app/src/main/java/com/example/tossapp/ui/screens/TabTossStockHome.kt
@@ -1,38 +1,13 @@
 package com.example.tossapp.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.tossapp.common.MenuTitle
-import com.example.tossapp.ui.components.CommonSpacer
-import com.example.tossapp.ui.components.MyAccount
-import com.example.tossapp.ui.components.MyInvest
-import com.example.tossapp.ui.components.MyInvestList
+import com.example.tossapp.common.CommonSpacer
 import com.example.tossapp.ui.screens.section.Section1
 import com.example.tossapp.ui.screens.section.Section2
 import com.example.tossapp.ui.screens.section.Section3
@@ -41,7 +16,6 @@ import com.example.tossapp.ui.screens.section.Section5
 import com.example.tossapp.ui.screens.section.Section6
 import com.example.tossapp.ui.screens.section.Section7
 import com.example.tossapp.ui.screens.section.Section8
-import com.example.tossapp.ui.theme.baseColor
 
 @Composable
 fun TossStockHome() {
